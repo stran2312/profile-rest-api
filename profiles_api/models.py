1245b9593c670ad9290f8f2352da73e42cc65f06
Sang Tran <fyc585@my.utsa.edu>
2022-08-10 02:49:09 -0500

    Added custom user profile mode, manager and migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,25 +31,22 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database Model for users in the system"""
+    """DB model for users"""
     email = models.EmailField(max_length=255, unique=True)
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
-    USERNAME_FILED = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user """
         return self.name
 
     def get_short_name(self):
-        """retriev short name of user"""
         return self.name
 
     def __str__(self):
-        """Rerturn string representation of our user """
         return self.email

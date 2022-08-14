4eb7ce61a133df49660614dd617fa0a6b487d522
Sang Tran <fyc585@my.utsa.edu>
2022-08-14 02:42:16 -0500

    Configured for deployment.

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/stran2312/profile-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
 # Set Ubuntu Language
